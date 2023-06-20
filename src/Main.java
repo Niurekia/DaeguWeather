8cac9c3c4e67a4556e6ded74ac721451816c4fbd
Niurekia <Niurekia@gmail.com>
2023-06-20 14:47:04 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("start");
+        int i=0;
+        for (int j = 0; j < 5; j++) {
+            System.out.println(i+j);
+        }
     }
 }
